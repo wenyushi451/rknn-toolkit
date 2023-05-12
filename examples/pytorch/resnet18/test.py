615caadfa724cba3482a292c8f02ba4f1ab060b2
shiwy <wenyushi451@gmail.com>
2023-05-13 04:24:49 +0800

    edge_pair load model successfully

--- examples/pytorch/resnet18/test.py
+++ examples/pytorch/resnet18/test.py
@@ -1,15 +1,15 @@
 import numpy as np
 import cv2
 from rknn.api import RKNN
-import torchvision.models as models
+# import torchvision.models as models
 import torch
 
 
-def export_pytorch_model():
-    net = models.resnet18(pretrained=True)
-    net.eval()
-    trace_model = torch.jit.trace(net, torch.Tensor(1,3,224,224))
-    trace_model.save('./resnet18.pt')
+# def export_pytorch_model():
+#     net = models.resnet18(pretrained=True)
+#     net.eval()
+#     trace_model = torch.jit.trace(net, torch.Tensor(1,3,224,224))
+#     trace_model.save('./resnet18.pt')
 
 
 def show_outputs(output):
@@ -39,18 +39,24 @@ def softmax(x):
 
 
 if __name__ == '__main__':
+    assert torch.__version__.startswith("1.10")
 
-    export_pytorch_model()
+    # export_pytorch_model()
 
-    model = './resnet18.pt'
-    input_size_list = [[3, 224, 224]]
+    # model = './resnet18.pt'
+    # model = "/home/wenyu/fabric-ml/cloud/models/pytorch1.10/scriptmodule_cpu.pt"
+    # model = "/home/wenyu/fabric-ml/cutpaste/models/model.pt"
+    # model = "/home/wenyu/fabric-ml/cloud/outputs_edge/2023-05-11/02-49-31/traced.pt"
+    model = "/home/wenyu/fabric-ml/cloud/outputs_edge/2023-05-12/03-36-00/traced.pt"
+    input_size_list = [[3, 256, 256]]
 
     # Create RKNN object
     rknn = RKNN()
 
     # pre-process config
     print('--> Config model')
-    rknn.config(mean_values=[[123.675, 116.28, 103.53]], std_values=[[58.395, 58.395, 58.395]], reorder_channel='0 1 2')
+    rknn.config(reorder_channel='1 2 3', mean_values=[[0, 0, 0]],
+                std_values=[[1, 1, 1]],)
     print('done')
 
     # Load Pytorch model
@@ -63,7 +69,7 @@ if __name__ == '__main__':
 
     # Build model
     print('--> Building model')
-    ret = rknn.build(do_quantization=True, dataset='./dataset.txt')
+    ret = rknn.build(do_quantization=False) # to match up with pytorch, don't enable do_quantization
     if ret != 0:
         print('Build model failed!')
         exit(ret)
@@ -81,7 +87,10 @@ if __name__ == '__main__':
 
     # Set inputs
     img = cv2.imread('./space_shuttle_224.jpg')
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (256, 256))
+    # edge_pair_input = np.random.randn((2, 3, 256, 256))
+    # edge_piar_mask = np.zeros((1, 1281, 1281), dtype=bool)
 
     # Init runtime environment
     print('--> Init runtime environment')
@@ -94,8 +103,12 @@ if __name__ == '__main__':
     # Inference
     print('--> Running model')
     outputs = rknn.inference(inputs=[img])
+    # outputs = rknn.inference(inputs=[edge_pair_input, edge_piar_mask])
     print(outputs)
-    show_outputs(softmax(np.array(outputs[0][0])))
-    print('done')
-
     rknn.release()
+
+    # rknn and pytorch should match
+    pytorch_model = torch.jit.load(model)
+    pytorch_model.eval()
+    input_tensor = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0)
+    print(pytorch_model(input_tensor))
